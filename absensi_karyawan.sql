2e3cb33cbd4f4b7662217634b577c8dde998941b
RepAb <abid.abiyyu490@smk.belajar.id>
2024-12-06 10:58:17 +0700

    edit sql

--- absensi_karyawan.sql
+++ absensi_karyawan.sql
@@ -5,6 +5,20 @@
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `absensi`
+--
+
+CREATE TABLE `absensi` (
+  `id_absensi` int NOT NULL,
+  `user_id` int NOT NULL,
+  `jam_masuk` time NOT NULL,
+  `jam_keluar` time NOT NULL,
+  `tangga_absen` date NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `divisi`
 --
 
@@ -16,23 +30,43 @@ CREATE TABLE `divisi` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `jam_kerja`
+--
+
+CREATE TABLE `jam_kerja` (
+  `id_jam` int NOT NULL,
+  `jam_masuk` int NOT NULL,
+  `jam_keluar` int NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id_user` int NOT NULL,
-  `nik` varchar(255) NOT NULL,
   `nama_lengkap` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `level` enum('User','Admin') NOT NULL,
   `divisi_id` int NOT NULL,
-  `password` varchar(255) NOT NULL
+  `jam_id` int NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `absensi`
+--
+ALTER TABLE `absensi`
+  ADD PRIMARY KEY (`id_absensi`),
+  ADD KEY `absensi_user` (`user_id`);
 
 --
 -- Indexes for table `divisi`
@@ -41,21 +75,40 @@ ALTER TABLE `divisi`
   ADD PRIMARY KEY (`id_divisi`);
 
 --
+-- Indexes for table `jam_kerja`
+--
+ALTER TABLE `jam_kerja`
+  ADD PRIMARY KEY (`id_jam`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`),
-  ADD KEY `divisi_id` (`divisi_id`);
+  ADD KEY `user_jam` (`jam_id`),
+  ADD KEY `user_divisi` (`divisi_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `absensi`
+--
+ALTER TABLE `absensi`
+  MODIFY `id_absensi` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `divisi`
 --
 ALTER TABLE `divisi`
   MODIFY `id_divisi` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jam_kerja`
+--
+ALTER TABLE `jam_kerja`
+  MODIFY `id_jam` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -68,8 +121,15 @@ ALTER TABLE `users`
 --
 
 --
+-- Constraints for table `absensi`
+--
+ALTER TABLE `absensi`
+  ADD CONSTRAINT `absensi_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`divisi_id`) REFERENCES `divisi` (`id_divisi`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `user_divisi` FOREIGN KEY (`divisi_id`) REFERENCES `divisi` (`id_divisi`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `user_jam` FOREIGN KEY (`jam_id`) REFERENCES `jam_kerja` (`id_jam`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
