be783d62e2774c3aec3c304b79d7b2439ce220e0
AbidAbiyyuImani <abid.abiyyu490@smk.belajar.id>
2024-12-24 08:52:43 +0700

    edit

--- pages/hapus_absensi_cuti.php
+++ pages/hapus_absensi_cuti.php
@@ -1,16 +1,22 @@
 <?php include 'config/functions.php';
-// redirect user (admin only)
-if ($_SESSION['pengguna']['level'] === 'User') { echo "<script>alert('Hanya admin yang dapat mengakses');location.href='index.php';</script>"; };
-
-$idCuti = $_GET['id'];
-try {
-  $queryDelete = querySQL("DELETE FROM absensi_cuti WHERE id_cuti = $idCuti");
-  if($queryDelete) {
-    echo "<script>alert('Hapus Data Cuti Berhasil'); location.href='?page=data_cuti';</script>";
+// mengalihkan karyawan ke halaman utama
+if ($_SESSION['pengguna']['level'] === 'Karyawan') {
+  echo "<script>alertPopUp('index.php', 'error', 'Gagal', 'Anda tidak memiliki akses ke halaman ini.');</script>";
+} else {
+  if (isset($_GET['id'])) {
+    try {
+      $idCuti = $_GET['id'];
+      $queryDelete = querySQL("DELETE FROM absensi_cuti WHERE id_absensi_cuti = $idCuti");
+      if ($queryDelete) {
+        echo "<script>alertPopUp('?page=data_absensi_cuti', 'success', 'Berhasil menghapus data permohonan cuti', 'Mengalihkan ke halaman data cuti...');</script>";
+      } else {
+        echo "<script>alertPopUp('?page=data_absensi_cuti', 'error', 'Gagal menghapus data permohonan cuti', 'Mengalihkan ke halaman data cuti...');</script>";
+      }
+    } catch (Exception $e) {
+      echo "<script>alertPopUp('?page=data_absensi_cuti', 'warning', 'Tidak dapat menghapus data permohonan cuti', 'Mengalihkan ke halaman data cuti...');</script>";
+    }
   } else {
-    echo "<script>alert('Hapus Data Cuti Gagal');</script>";
+    echo "<script>alertPopUp('?page=data_absensi_cuti', 'error', 'Tidak ada data permohonan cuti yang dipilih', 'Mengalihkan ke halaman data cuti...');</script>";
   }
-} catch (Exception $e) {
-  echo "<script>alert('Hapus Data Cuti Gagal');location.href='?page=data_cuti';</script>";
 }
 ?>
